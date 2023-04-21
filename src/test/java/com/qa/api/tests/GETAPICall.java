ba7359f3474fa33995853a93e618944a6984c2cb
Igor Ostojic <ostojic.igor0@gmail.com>
2023-04-21 15:29:37 +0200

    01-getrequests wip

--- src/test/java/com/qa/api/tests/GETAPICall.java
+++ src/test/java/com/qa/api/tests/GETAPICall.java
@@ -1,2 +1,43 @@
-package com.qa.api.tests;public class GETAPICall {
+package com.qa.api.tests;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsoft.playwright.APIRequest;
+import com.microsoft.playwright.APIRequestContext;
+import com.microsoft.playwright.APIResponse;
+import com.microsoft.playwright.Playwright;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GETAPICall {
+
+    @Test
+    public void getUserApiTest() throws IOException {
+
+        Playwright playwrigt = Playwright.create();
+        APIRequest request = playwrigt.request();
+        APIRequestContext requestContext = request.newContext();
+        APIResponse apiResponse = requestContext.get("https://gorest.co.in/public/v2/users");
+
+//        Catch status code
+        int statusCode = apiResponse.status();
+        System.out.println("response status code:" + statusCode);
+        Assert.assertEquals(statusCode, 200);
+
+//        Catch status text
+        String statusText = apiResponse.statusText();
+        System.out.println(statusText);
+
+//        Catch body
+        apiResponse.body();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonResponse = objectMapper.readTree(apiResponse.body());
+        String jsonPrettyResponse = jsonResponse.toPrettyString();
+        System.out.println(jsonPrettyResponse);
+
+
+    }
+
 }
